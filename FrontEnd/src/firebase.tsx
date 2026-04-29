dc3e382302c5fe2dcc0ca72e8f08a96e64580e1f
JustinSufrapto <justinsufrapto@gmail.com>
2026-04-29 20:31:30 +0700

    Update Firebase API key in firebase.tsx
    Replaced the Firebase API key with a hardcoded value.

--- FrontEnd/src/firebase.tsx
+++ FrontEnd/src/firebase.tsx
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    apiKey: "AIzaSyCkpcbGRcPLYZPaX4sYD8wS5WKcnd_dn4M",
     authDomain: "webaicalo.firebaseapp.com",
     projectId: "webaicalo",
     storageBucket: "webaicalo.firebasestorage.app",
